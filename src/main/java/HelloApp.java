ba3d92f2bd1b944b058478a936446712925a5530
nadha263 <sanoojmohamed75@gmail.com>
2026-03-28 23:46:01 +0530

    uc4

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,6 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }}}}
