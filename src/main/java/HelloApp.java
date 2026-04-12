ffea754bc535ed06330fbd20092e97b1ab2871a1
nadha263 <sanoojmohamed75@gmail.com>
2026-04-13 01:24:43 +0530

    uc7

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,9 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        String greeting;
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            greeting = "Hello, " + String.join(", ", args) + "!";}
+        System.out.println(greeting);
+    }}
