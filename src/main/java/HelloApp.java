a4a7ccdb2fd036de78ef9682fd62f7e64ed5c6d8
nadha263 <sanoojmohamed75@gmail.com>
2026-04-13 01:24:43 +0530

    uc7

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,25 +1,11 @@
 
 public class HelloApp {
-
     public static void main(String[] args) {
-
-    if (args.length == 0) {
-        System.out.println("Hello, World!");
-        return;
-    }
-
-    StringBuilder nameBuilder = new StringBuilder();
-
-    for (String name : args) {
-        nameBuilder.append(name).append(", ");
-    }
-
-    String names = "";
-    if (nameBuilder.length() > 0) {
-        names = nameBuilder.substring(0, nameBuilder.length() - 2);
-    }
-
-    System.out.println("Hello, " + names + "!");
-}
-}
+        String greeting;
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            greeting = "Hello, " + String.join(", ", args) + "!";}
+        System.out.println(greeting);
+    }}
 
